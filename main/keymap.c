912ce4f95ba72f7e894a7f04323bcb1b9380a7b2
Jorge Ibacache <jo.ibacache@gmail.com>
2021-07-28 09:31:28 -0400

    minor changes on keymap

--- main/keymap.c
+++ main/keymap.c
@@ -99,19 +99,19 @@ uint16_t default_slave_encoder_map[LAYERS][ENCODER_SIZE] = {
 
 		/* Lower
 		* ,-----------------------------------------------------------------------------------.
-		* |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+		* |   ~  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
 		* |------+------+------+------+------+------+------+------+------+------+------+------|
-		* | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+		* | Caps |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
 		* |------+------+------+------+------+------+------+------+------+------+------+------|
-		* | Caps |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      | Home |      | End  |
+		* | Shift|  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      | Home |      | End  |
 		* |------+------+------+------+------+------+------+------+------+------+------+------|
 		* | DFLT |      |      |      |      |     Play    |      | Prev | Vol- | Vol+ | Next |
 		* `-----------------------------------------------------------------------------------'
 		*/
 
-		{ KC_TRNS, KC_ALT_F4, KC_8, KC_7, KC_6, KC_5, KC_4, KC_3, KC_2, KC_1, KC_0, KC_TRNS },
-		{ KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
-		{ KC_CAPS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NONE, KC_NONE, KC_HOME, KC_NONE,  KC_END },
+		{ KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NONE },
+		{ KC_CAPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
+		{ KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NONE, KC_NONE, KC_HOME, KC_NONE,  KC_END },
 		{ DEFAULT, KC_NONE, KC_NONE, KC_NONE, KC_NONE,      KC_MPLY, KC_MPLY,     KC_NONE,    KC_MPRV,    KC_VOLD, KC_VOLU, KC_MNXT }
 
 	 };
@@ -124,7 +124,7 @@ uint16_t default_slave_encoder_map[LAYERS][ENCODER_SIZE] = {
 				* |------+------+------+------+------+------+------+------+------+------+------+------|
 				* | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
 				* |------+------+------+------+------+------+------+------+------+------+------+------|
-				* | Caps |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | Del  | Bksp |Pg Up |      |Pg Dn |
+				* | Shift |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | Del  | Bksp |Pg Up |      |Pg Dn |
 				* |------+------+------+------+------+------+------+------+------+------+------+------|
 				* | DFLT |      |      |      |      |     Play    |      | Prev | Vol- | Vol+ | Next |
 				* `-----------------------------------------------------------------------------------'
@@ -132,7 +132,7 @@ uint16_t default_slave_encoder_map[LAYERS][ENCODER_SIZE] = {
 
 				{ KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NONE },
 				{ KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS },
-				{ KC_CAPS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, KC_PGUP, KC_NO, KC_PGDN },
+				{ KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, KC_PGUP, KC_NO, KC_PGDN },
 				{ DEFAULT, KC_NONE, KC_NONE, KC_NONE, KC_NONE,      KC_MPLY,KC_MPLY,     KC_NONE, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT }
 
 	};
